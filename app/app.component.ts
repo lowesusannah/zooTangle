b46a57f3ea8dd6ed9da35aec80c11cb92851120d
Emily Wells Jimenez and Susannah Lowe <emily+susannah@>
2017-12-06 11:49:45 -0800

    add styling and begin setting app component and class.

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,7 +8,11 @@ import { Component } from '@angular/core';
     <h3>Look at all the beers we have!</h3>
   </div>
   <div class="beer-container">
-    <div class="beer" *ngFor="let currentKeg of kegs">{{currentKeg.name}}
+    <div class="beer" *ngFor="let currentKeg of kegs">{{currentKeg.name}}, {{currentKeg.brand}}, ABV: {{currentKeg.abv}}, {{currentKeg.pints}} pints left, {{currentKeg.price}},
+    <button (click)="pourBeer(currentKeg)" type="button" name="pour">Pour Beer</button>
+    <button type="button" name="edit">Edit Keg</button>
+    <div [class]="beerImg(currentKeg)">
+    </div>
     </div>
   </div>
    `
@@ -16,16 +20,27 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
 
-kegs: Keg[] = [
-  new Keg("Black Butte Porter", "Deschutes Brewery", "$8", "5.2%"),
-  new Keg("Jubelale", "Deschutes Brewery", "$7", "6.7%"),
-  new Keg("Lost Meridian", "Basecamp Brewing", "$6.50", "5.0%"),
-  new Keg("Hefe", "Widmer Brothers Brewing", "$5", "4.9%"),
-]
+public kegs: Keg[] = [
+  new Keg("Black Butte Porter", "Deschutes Brewery", "$8", 5.2),
+  new Keg("Jubelale", "Deschutes Brewery", "$7", 6.7),
+  new Keg("Lost Meridian", "Basecamp Brewing", "$6.50", 5.0),
+  new Keg("Hefe", "Widmer Brothers Brewing", "$5", 4.9),
+];
 
- }
+beerImg(currentKeg) {
+  if (currentKeg.abv <= 5) {
+    return "beer-light";
+  } else {
+    return "beer-dark";
+  }
+}
 
+pourBeer(currentKeg) {
+  return currentKeg.pints--;
+}
+
+}
 export class Keg {
   public pints: number = 124;
-  constructor(public name: string, public brand: string, public price: string, public abv: string){};
+  constructor(public name: string, public brand: string, public price: string, public abv: number){ };
 }
