8dc299b3634e35f00f88c299a485a5c385c271a0
Emily Wells Jimenez and Susannah Lowe <emily+susannah@>
2017-12-06 16:44:07 -0800

    add more methods and styling.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,17 +4,36 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="title">
-    <h1>Welcome to the Tap Room</h1>
-    <h3>Look at all the beers we have!</h3>
+    <h1>Welcome to Tap Works</h1>
+    <h3>Check out what we have on tap!</h3>
   </div>
   <div class="beer-container">
-    <div class="beer" *ngFor="let currentKeg of kegs">{{currentKeg.name}}, {{currentKeg.brand}}, ABV: {{currentKeg.abv}}, {{currentKeg.pints}} pints left, {{currentKeg.price}},
-    <button (click)="pourBeer(currentKeg)" type="button" name="pour">Pour Beer</button>
-    <button type="button" name="edit">Edit Keg</button>
-    <div [class]="beerImg(currentKeg)">
-    </div>
+    <div class="beer" *ngFor="let currentKeg of kegs">
+      <div class="info">
+      {{currentKeg.name}}, {{currentKeg.brand}}, ABV: {{currentKeg.abv}}, {{currentKeg.pints}} pints left, {{currentKeg.price}}
+      </div>
+      <div [class]="beerImg(currentKeg)">
+      </div>
+    <button (click)="pourBeer(currentKeg)" type="button" name="pour">Beer Me!</button>
+    <button (click)="pourGrowler(currentKeg)" type="button" name="pour">Pour Growler</button>
+    <button (click)="editKeg(currentKeg)" type="button" name="edit">Edit Keg</button>
+    <button type="button" name="delete">Delete Keg</button>
     </div>
   </div>
+  <button type="button" name="add">Add Keg</button>
+  <div class="form" *ngIf="selectedKeg">
+    <h3>Edit {{selectedKeg.name}} Keg</h3>
+    <label>Edit Name:</label>
+    <input [(ngModel)]="selectedKeg.name">
+    <label>Edit Brand:</label>
+    <input [(ngModel)]="selectedKeg.brand">
+    <label>Edit ABV:</label>
+    <input [(ngModel)]="selectedKeg.abv">
+    <label>Edit Price:</label>
+    <input [(ngModel)]="selectedKeg.price">
+    <br>
+    <button (click)="finishedEditing()">Done</button>
+   </div>
    `
 })
 
@@ -27,6 +46,17 @@ public kegs: Keg[] = [
   new Keg("Hefe", "Widmer Brothers Brewing", "$5", 4.9),
 ];
 
+selectedKeg = null;
+
+editKeg(clickedKeg) {
+  this.selectedKeg = clickedKeg;
+}
+
+finishedEditing() {
+  this.selectedKeg = null;
+}
+
+
 beerImg(currentKeg) {
   if (currentKeg.abv <= 5) {
     return "beer-light";
@@ -38,6 +68,10 @@ beerImg(currentKeg) {
 
 pourBeer(currentKeg) {
   return currentKeg.pints--;
+}
+
+pourGrowler(currentKeg) {
+  return currentKeg.pints-=2;
 }
 
 }
